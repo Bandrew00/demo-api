802b2708e24e3724bd7402e952863e72d4e3fc19
andrea <andrea@CONSOLLE.Drake.Local>
2021-10-14 11:31:33 +0200

    update

--- nic_info.py
+++ nic_info.py
@@ -1,10 +1,35 @@
 from flask import Flask, request
-import json, logging, os, sys
+import json, logging, os# , sys
 
-c_host 		= None  # Server flask
-c_ddns_folder	= 'DDNS/'
-c_setup_folder  = 'SETUP-DDNS/'
-app 		= Flask(__name__)
+c_host 		    = "localhost" # None  # Server flask
+c_ddns_folder	= (os.path.expanduser('~'))+"/DDNS-CONFIG/"
+c_nic_info_url  = 'http://{}:8080/Info?id={}' 
+#c_setup_folder = 'SETUP-DDNS/'
+def TestFolder(folder,l_folder):
+    
+    for subdir, dirs, files in os.walk(folder):
+        
+       # for f in files:
+       #     l_file.append(f)
+       #     pass 
+        
+        for folder in dirs:   
+            l_file = [f for f in os.listdir(os.path.join(subdir,folder))]
+            if len(l_file)>0:
+                d_file= {}
+                d_folder= {}
+                for file in l_file :
+                    d_file[file]=c_nic_info_url.format(c_host,os.path.join(subdir,folder))
+                
+                d_folder[folder] = d_file
+                l_folder.append(d_folder)
+            else:l_folder.append(folder)
+            
+            
+            TestFolder(os.path.join(subdir,folder),l_folder) 
+    pass
+
+app = Flask(__name__)
 
 ################################################################
 # Pagina iniziale
@@ -15,18 +40,27 @@ def Info():
         d_folder = {}
         d_item   = {}
         l_folder = []
+        ddns_folder=[]
         nic_info_url = 'http://{}:8080/Info?id={}' 
         # ricavo la lista delle cartelle in DDNS
-        for folder in next(os.walk(c_ddns_folder))[1]:
-            d_item[folder]= nic_info_url.format(c_host,folder)
- 	    l_folder.append(d_item) 
-            d_item={}
-
-        d_folder =  {"NIC LIST" : l_folder }
-        nic_list = json.dumps( d_folder,indent=4)
+        # for folder in next(os.walk(c_ddns_folder))[1]:
+        #     d_item[folder]= nic_info_url.format(c_host,folder)
+        #     l_folder.append(d_item) 
+        #     d_item={}
+       
+        for folder in next(os.walk(c_ddns_folder))[1] :
+                       
+            TestFolder(os.path.join(c_ddns_folder,folder),l_folder)
+            d_item[folder]=l_folder
+            ddns_folder.append(d_item)
+            l_folder = []
+            d_item   ={}
+        
+        d_folder =  {"DDNS LIST" : l_folder }
+        nic_list = json.dumps( ddns_folder,indent=4)
         return nic_list,200
     except (Exception) as e:
-	return "DDNS Fail: {}".format(e), 404
+	    return "DDNS Fail: {}".format(e), 404
 
 ################################################################
 # Ottieni il contenuto del file JSON da visualizzare
@@ -40,77 +74,75 @@ def Get():
         json_file = c_ddns_folder + id + '/data.json'
     	# Opening JSON file
     	with open(json_file, 'r') as f:
-	     json_data = json.load(f)
+	        json_data = json.load(f)
     	json_formatted_str = json.dumps(json_data,indent=4)
  	return json_formatted_str,200
     except (Exception) as e:
-	return "DDNS Fail: {}".format(e), 404
+	    return "DDNS Fail: {}".format(e), 500
 ################################################################
 # Copia e sostituisci file JSON su C_HOST
 ################################################################
-@app.route("/Info", methods=["POST"])
-def Post():
-    try:
-    	json_data = request.json
-    	# Prelevo chiave composta da NAME + LAN per ricavare la cartella in DDNS
-    	nic_name   = json_data["NAME"].replace('\n','')
-    	nic_lan    = json_data["LAN"].replace('\n','')
-    	nic_folder = c_ddns_folder + nic_name + '-' + nic_lan
-    	# Path dove scaricare il Data JSON
-    	if not os.path.exists(nic_folder):os.makedirs(nic_folder)
-    	with open(nic_folder + '/data.json', 'w') as f:
-             json.dump(json_data, f, indent=4)
-    	return "OK",201
-    except (Exception) as e:
-	return "DDNS Fail: {}".format(e), 500
-
+##@app.route("/Info", methods=["POST"])
+##def Post():
+##    try:
+##    	json_data = request.json
+##    	# Prelevo chiave composta da NAME + LAN per ricavare la cartella in DDNS
+##    	nic_name   = json_data["NAME"].replace('\n','')
+##    	nic_lan    = json_data["LAN"].replace('\n','')
+##    	nic_folder = c_ddns_folder + nic_name + '-' + nic_lan
+##    	# Path dove scaricare il Data JSON
+##    	if not os.path.exists(nic_folder):os.makedirs(nic_folder)
+##    	with open(nic_folder + '/data.json', 'w') as f:
+##             json.dump(json_data, f, indent=4)
+##    	return "OK",201
+##    except (Exception) as e:
+##	    return "DDNS Fail: {}".format(e), 500
+##
 ################################################################
 # Creo struttura ad albero su file system (Mappatura NUC + NIC)
 ################################################################
-@app.route("/Add", methods=["POST"])
-def Add():
-    try:
-    	json_data    = request.json
-    	# Prelevo chiave NUC per creare la cartella radice in SETUP-DDNS
-        nuc_name_key = json_data.keys()[1] #.replace('\n','')
-  	nuc_folder   = c_setup_folder + nuc_name_key + '/'
-	if not os.path.exists(nuc_folder):os.makedirs(nuc_folder)
-	# ciclo la lista delle nic con chiave nuc_name_key
-        # e creo struttura su file system
-        for nic_name in json_data[nuc_name_key]:
- 	    nic_folder = nuc_folder + nic_name
-            if not os.path.exists(nic_folder):os.makedirs(nic_folder)
-	return "OK",201
-    except (Exception) as e:
-	return "DDNS Fail: {}".format(e), 500
-
-
-
-
+##@app.route("/Add", methods=["POST"])
+##def Add():
+##    try:
+##        json_data    = request.json
+##    	# Prelevo chiave NUC per creare la cartella radice in SETUP-DDNS
+##        nuc_name_key = json_data.keys()[1] #.replace('\n','')
+##        nuc_folder   = c_setup_folder + nuc_name_key + '/'
+##        if not os.path.exists(nuc_folder):os.makedirs(nuc_folder)
+##	    # ciclo la lista delle nic con chiave nuc_name_key
+##        # e creo struttura su file system
+##        for nic_name in json_data[nuc_name_key]:
+##            nic_folder = nuc_folder + nic_name
+##            if not os.path.exists(nic_folder):os.makedirs(nic_folder)
+##        return "OK",201
+##    except (Exception) as e:
+##        return "DDNS Fail: {}".format(e), 500
+##	  
+##
+##
+##
 if __name__ == '__main__':
 
-    NUM_PARAMS 	= 1
-    ERROR 	= False
-    error       = None
+    #NUM_PARAMS 	= 1
+    #ERROR 	    = False
+    #error       = None
     logging.basicConfig(filename='nic_info.log',
                         filemode='a', #'w',
                         level=logging.ERROR,
                         format='%(levelname)s: %(asctime)s: %(message)s') 
+    
     try:
-       if ( len(sys.argv)-1 < NUM_PARAMS ):
-          error ="DDNS Fail: numero insufficiente di parametri!\nUso: python nic_inf <parameter1> (Web API Host Ip es. 10.26.26.13)"
-	  ERROR = True
-       else:
-          c_host = str(sys.argv[1])
-    	  # Path dove scaricare il Data JSON
-    	  if not os.path.exists("./" + c_ddns_folder):
-	     os.makedirs(c_ddns_folder)
-    	  app.run(host=c_host,port=8080)
+        pass
+        # if ( len(sys.argv)-1 < NUM_PARAMS ):
+        #    error ="DDNS Fail: numero insufficiente di parametri!\nUso: python nic_inf <parameter1> (Web API Host Ip es. 10.26.26.13)"
+        #    ERROR = True
+        # else:
+        #    c_host = str(sys.argv[1])
+        #    # Path dove scaricare il Data JSON
+        if not os.path.exists(c_ddns_folder):
+	            os.makedirs(c_ddns_folder)
+    	app.run(host=c_host,port=8080)
     except (Exception) as e:
- 	  error = "DDNS Fail: {}".format(e)
-          ERROR = True
+        pass
     finally:
-          if ERROR:
-             print(error)
-             logging.error(error)
-             sys.exit(1)
+        pass
--- setup-nuc.py
+++ setup-nuc.py
@@ -1,6 +1,4 @@
-import subprocess as sp
-import logging, os, sys
-
+#
 # LO SCRIPT CREA SU FILE SYSTEM,UNA STRUTTURA DI CARTELLE AVENDO COME RADICE
 # LA CARTELLA 'DDNS CONFIG'.
 # COME PARAMETRO DI INPUT ABBIAMO L'INDIRIZZO IP DELLA NUC DA CONFIGURARE.
@@ -27,55 +25,53 @@ import logging, os, sys
 # FILE YAML DI CONFIGURAZIONE PER L'AGGIORNAMENTO DDNS PROCESSATO 
 # DALLO SCRIPT './demo/update.py'
 #
-c_ddns_config_folder = './DDNS-CONFIG/'
-c_nuc_ssh_user 	     = "tiesse@"
+#
+import subprocess as sp
+import logging, os, sys
 
-C_NUM_PARAMS 	     = 1
-ERROR 	             = False
-error                = "DDNS Fail: numero insufficiente di parametri!\nUso: python setup-nuc.py <parameter1> (parameter1=Indirizzo IP NUC: es 192.168.197.10)"
+c_ddns_folder  = (os.path.expanduser('~'))+"/2DDNS-CONFIG/"
+c_nuc_ssh_user = "tiesse@"
+c_num_params   = 1
+ERROR 	      = False
+error          = "DDNS Fail: numero insufficiente di parametri!\nUso: python setup-nuc.py <parameter1> (parameter1=Indirizzo IP NUC: es 192.168.197.10)"
 
 if __name__ == "__main__":
-
-
-   logging.basicConfig(filename='setup-nuc.log',
+   logging.basicConfig(filename='setup_nuc.log',
                         filemode='a', #'w',
                         level=logging.INFO,
                         format='%(levelname)s: %(asctime)s: %(message)s') 
+
    try:
-       if ( len(sys.argv)-1 < C_NUM_PARAMS ):
-	  ERROR = True
-       else:
-          nuc_ip       = str(sys.argv[1])                                      # parametro indirizzo NUC IP
-	  ssh_command  = 'ssh ' + c_nuc_ssh_user + nuc_ip                      # comando SSH con credenziali utente NUC (ssh tiesse@xx.xx.xx.xx)
-          #
-          # Path NUC
-          #
-          nuc_hostname = sp.check_output(ssh_command + ' hostname',shell=True) # ricavo nome host della nuc
- 	  nuc_folder   = nuc_hostname.replace('\n','')+"_"+nuc_ip+'/'          # cartella NUC da creare
-          # 
-          # ricavo indirizzi NIC dalla tabella di routing della NUC
-          #
-          cmd = ssh_command + ' ip r' + ' | awk \'{ print $1 }\' | grep -i \'/30\''
-          for ip in sp.check_output(cmd,shell=True).splitlines():
-	      ip         = ip[0:ip.rfind('/')]
-              old_digit  = ip[ip.rfind('.')+1:]                                # ultime cifra indirizzo IP xx.xx.xx.nn
-              new_digit  = str(int(old_digit) +2)                              # aggiungo 2 all'ottetto lsb per ottenere IP NIC
-              ip         = ip.replace('.'+old_digit, '.'+new_digit)
-              #
-              # Ottengo nome host della NIC via SSH
-              #
-              nic_hostname = sp.check_output(ssh_command + ' ssh root@' + ip + ' hostname',shell=True) # ricavo nome host della nic              
-              nic_folder   = nic_hostname.replace('\n','')+"_"+ip 
-              if not os.path.exists("./" + c_ddns_config_folder + nuc_folder + nic_folder):
-                 os.makedirs(c_ddns_config_folder + nuc_folder + nic_folder)
-                 logging.info("Nuova cartella: " + c_ddns_config_folder + nuc_folder + nic_folder) 
-                             
+      if ( len(sys.argv)-1 < c_num_params ):
+	      ERROR = True
+      else:
+         nuc_ip = str(sys.argv[1])                 # parametro indirizzo NUC IP
+         ssh    = 'ssh ' + c_nuc_ssh_user + nuc_ip # comando SSH con credenziali utente NUC (ssh tiesse@xx.xx.xx.xx)
+         #
+         # Path NUC
+         #
+         nuc_hostname = sp.check_output(ssh + ' hostname',shell=True) # ricavo nome host della nuc
+         nuc_folder   = nuc_hostname.replace('\n','')+"_"+nuc_ip+'/'  # cartella NUC da creare
+         #
+         # ricavo indirizzi NIC dalla tabella di routing della NUC
+         #
+         cmd = ssh + ' ip r' + ' | awk \'{ print $1 }\' | grep -i \'/30\''
+         for ip in sp.check_output(cmd,shell=True).splitlines():
+            ip         = ip[0:ip.rfind('/')]
+            old_digit  = ip[ip.rfind('.')+1:]    # ultima cifra indirizzo IP xxx.xxx.xxx.nnn
+            new_digit  = str(int(old_digit) +2)  # aggiungo 2 all'ottetto lsb per ottenere IP NIC
+            ip         = ip.replace('.'+old_digit, '.'+new_digit)
+            #
+            # Ottengo nome host della NIC via SSH
+            #
+            nic_hostname = sp.check_output(ssh + ' ssh root@' + ip + ' hostname',shell=True)
+            nic_folder   = nic_hostname.replace('\n','')+"_"+ip 
+            if not os.path.exists("./" + c_ddns_folder + nuc_folder + nic_folder):
+               os.makedirs(c_ddns_folder + nuc_folder + nic_folder)
+               logging.info("Nuova cartella: " + c_ddns_folder + nuc_folder + nic_folder)
+            pass
    except (Exception) as e:
- 	  error = "DDNS Fail: {}".format(e)
-          ERROR = True
+      pass
    finally:
-          if ERROR:
-             print(error)
-             logging.error(error)
-             sys.exit(1)
+      pass
 
